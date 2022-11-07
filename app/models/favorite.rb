0c2d9327a80c57325285d87a8394245b210bc532
00komiya <114364060+00komiya@users.noreply.github.com>
2022-11-07 08:59:18 +0000

    [add]7a

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,9 @@
 class Favorite < ApplicationRecord
-  
+
   belongs_to :user
   belongs_to :book
+
+  # ひとりのユーザー(user_id)がひとつのbook_idに対して1いいね
+  validates_uniqueness_of :book_id, scope: :user_id
 
 end
